3db7b2279f721124d0e5ed24144920102822a420
Eiske Hashimoto <hashimoto.eiske@gmail.com>
2017-09-21 19:46:15 -0300

    Postei

---
+++ RestricaoIntegridade.sql
@@ -0,0 +1,35 @@
+﻿--drop table Aluno;
+
+/*create table Aluno (
+ codAluno integer,
+ nome varchar(50),
+ codCurso integer,
+ Primary key (codAluno),
+ Foreign key (codCurso) references Curso (codCurso)
+ on delete restrict 
+ on update restrict
+);*/
+
+/*create table Aluno (
+ codAluno integer,
+ nome varchar(50),
+ codCurso integer,
+ Primary key (codAluno),
+ Foreign key (codCurso) references Curso (codCurso)
+ on delete cascade 
+ on update set null
+);*/
+
+--insert into Curso values (1, 'Computacao'); -- 6
+
+--insert into Aluno values (1,'Eiske',1); -- 7
+
+--delete from curso; -- 8
+
+--update Curso set codCurso = 60; -- 9
+
+--update Curso set codCurso = 50; -- 12
+
+--select * from Aluno; -- 13
+
+--delete from aluno
